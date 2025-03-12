440c546ce04dbe92e2b9392355927ed547c64baa
Maciek Czerniec <6043082@mborijnland.nl>
2025-03-12 11:47:47 +0100

    Add files via upload

---
+++ Periode3-ProjectDishDev/reserveren.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <!-- Zorgt ervoor dat de pagina correct wordt weergegeven op mobiele apparaten. -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- SEO (search enige optimation) Metagegevens -->
+    <!-- Description voor zoekmachines en sociale media, omschrijft kort de inhoud van de pagina. -->
+    <meta name="description" content="">
+    <!-- Keywords helpt zoekmachines begrijpen waar de pagina over gaat. -->
+    <meta name="Keywords" content="vegan restaurant amsterdam, plantaardig eten amsterdam, vegan food amsterdam, veganistisch restaurant amsterdam, beste vegan restaurant amsterdam, 
+    gezond vegan eten amsterdam, vegan brunch amsterdam, vegan take-away amsterdam, vegan fine dining amsterdam, biologisch vegan restaurant amsterdam, 
+    duurzaam eten amsterdam, vegan friendly restaurant amsterdam, vegan burgers amsterdam, vegan sushi amsterdam, vegan pizza amsterdam, vegan desserts amsterdam, 
+    vegan ontbijt amsterdam, vegan lunch amsterdam, vegan diner amsterdam, vegan restaurant in het centrum van amsterdam, vegan eetcafé amsterdam, vegan hotspot amsterdam, 
+    vegan restaurant jordaan, vegan eten in de pijp, vegan friendly cafés amsterdam">
+    <!-- Author de naam van de developer van de pagina. -->
+    <meta name="author" content="Noah Weerwag">
+
+    <title>Vegan Food // Reserveren</title>
+    <!-- Favicon kleine afbeelding die wordt weergegeven in de browser-tabbladen -->
+    <link rel="icon" href="images/beeldmerk_logo_vegan_food.png" type="image/x-icon">
+    <!-- Link naar stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!-- Link naar overige -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
