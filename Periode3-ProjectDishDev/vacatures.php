28f6dbf91d265bf0b13e6c35205547c417f5a677
6043082 <6043082@mborijnland.nl>
2025-03-28 14:00:54 +0100

    vacatures verbeterd
    betere versie van vacatures site

--- Periode3-ProjectDishDev/vacatures.php
+++ Periode3-ProjectDishDev/vacatures.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="nl">
+
 <head>
     <meta charset="UTF-8">
     <!-- Zorgt ervoor dat de pagina correct wordt weergegeven op mobiele apparaten. -->
@@ -23,50 +24,61 @@
     <link rel="icon" href="images/beeldmerk_logo_vegan_food.png" type="image/x-icon">
     <!-- Link naar stylesheet -->
     <link rel="stylesheet" href="css/style-MACIEK.css">
-    
+
     <!-- Link naar overige -->
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <header>
-        <?php include("header.php");?>
+        <?php include("header.php"); ?>
     </header>
-<main class="vacature-site">
-    <h1>-Vacatures-</h1>
-    <h2>Ons Vacatures</h2>
-    <section class="vacatures">
-        <article class="vacature">
-            <img src="https://luzt.nl/wp-content/uploads/1-e1685977147195.png" alt="Chefkok">
-            <h2>Chefkok</h2>
-            <p>Passie voor plantaardig koken? Als Vegan Chef-kok creëer je innovatieve gerechten en leid je het keukenteam.</p>
-            <button>Soliciteer Nu!</button>
-        </article>
-        <article class="vacature">
-            <img src="//mediabank.valkenhorst.nl/imagebank/images/360/846x511/image63552/c392x590.jpg" alt="Gastvrouw">
-            <h2>Gastvrouw</h2>
-            <p>Gastvrij, enthousiast en servicegericht? Als gastvrouw in ons vegan restaurant zorg je voor een warm welkom.</p>
-            <button>Soliciteer Nu!</button>
-        </article>
-        <article class="vacature">
-            <img src="https://www.entreemagazine.nl/hubfs/Afwassen%20-%20Header.png" alt="Afwasser">
-            <h2>Afwasser</h2>
-            <p>Als afwasser ben jij onmisbaar in onze keuken! Je zorgt voor schone borden, keukengerei en houdt alles netjes.</p>
-            <button>Soliciteer Nu!</button>
-        </article>
-        <article class="vacature">
-            <img src="https://www.upmenu.com/wp-content/uploads/2023/10/restaurant-assistant-manager-job-description-restaurant-assistant-manager.jpg" alt="Assistent Manager">
-            <h2>Manager</h2>
-            <p>Als manager ondersteun je de dagelijkse leiding van ons vegan restaurant en zorgt ervoor dat klanten tevreden zijn.</h2>
-            <br><br>
-            <button>Soliciteer Nu!</button>
-        </article>
-    </section>
-    <h5>Soliciteer Online of Contacteer Ons om meer Informatie te krijgen!</h5>
-</main>
+    <main class="vacature-site">
+        <h1>-Vacatures-</h1>
+        <h2>Onze Vacatures</h2>
+        <section class="vacatures">
+            <article class="vacature">
+                <img src="https://luzt.nl/wp-content/uploads/1-e1685977147195.png" alt="Chefkok">
+                <h2>Chefkok</h2>
+                <p>Passie voor plantaardig koken? Als Vegan Chef-kok creëer je innovatieve gerechten en leid je het
+                    keukenteam.</p>
+                <button>Soliciteer Nu!</button>
+            </article>
+            <article class="vacature">
+                <img src="//mediabank.valkenhorst.nl/imagebank/images/360/846x511/image63552/c392x590.jpg"
+                    alt="Gastvrouw">
+                <h2>Gastvrouw</h2>
+                <p>Gastvrij, enthousiast en servicegericht? Als gastvrouw in ons vegan restaurant zorg je voor een warm
+                    welkom.</p>
+                <button>Soliciteer Nu!</button>
+            </article>
+            <article class="vacature">
+                <img src="https://www.entreemagazine.nl/hubfs/Afwassen%20-%20Header.png" alt="Afwasser">
+                <h2>Afwasser</h2>
+                <p>Als afwasser ben jij onmisbaar in onze keuken! Je zorgt voor schone borden, keukengerei en houdt
+                    alles netjes.</p>
+                <button>Soliciteer Nu!</button>
+            </article>
+            <article class="vacature">
+                <img src="https://www.upmenu.com/wp-content/uploads/2023/10/restaurant-assistant-manager-job-description-restaurant-assistant-manager.jpg"
+                    alt="Assistent Manager">
+                <h2>Manager</h2>
+                <p>Als manager ondersteun je de dagelijkse leiding van ons vegan restaurant en zorgt ervoor dat klanten
+                    tevreden zijn.</h2>
+                    <br><br>
+                    <button>Soliciteer Nu!</button>
+            </article>
+        </section>
+        <h5>Soliciteer Online of Contacteer Ons om meer Informatie te krijgen!</h5>
+    </main>
     <footer>
-        <?php include("footer.php");?>
+        <?php include("footer.php"); ?>
     </footer>
 </body>
