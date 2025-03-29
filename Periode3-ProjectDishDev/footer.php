867cc43345df94250065eea86fa60261c7eaec8f
6043082 <6043082@mborijnland.nl>
2025-03-29 23:21:05 +0100

    css aangepast

--- Periode3-ProjectDishDev/footer.php
+++ Periode3-ProjectDishDev/footer.php
@@ -1,11 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Vegan Food // Footer</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/flag-icons.min.css">
 </head>
+
 <body>
-    
+    <footer>
+        <section class="footer-col">
+            <img src="images/beeldmerk_logo_vegan_food.png" alt="Vegan Food Amsterdam">
+        </section>
+        <section class="footer-col">
+            <h4>Informatie</h4>
+            <article><a href="#">Home</a></article>
+            <article><a href="#">Lunch & Diner</a></article>
+            <article><a href="#">Openingstijden & Locatie</a></article>
+            <article><a href="#">Reserveren</a></article>
+            <article><a href="#">Vacatures</a></article>
+        </section>
+        <section class="footer-col">
+            <h4>Openingstijden</h4>
+            <article class="tijd">
+                <p>Maandag - Zaterdag: 09:00 - 22:00</p>
+            </article>
+            <article class="tijd">
+                <p>Zondag: 10:00 - 20:00</p>
+            </article>
+        </section>
+        <section class="footer-col">
+            <h4>Contact</h4>
+            <article>
+                <p>Keizersgracht 212, 1016 DX Amsterdam</p>
+            </article>
+            <article>
+                <p>088 - 2321451</p>
+            </article>
+            <article>
+                <p>info@veganfoodamsterdam.nl</p>
+            </article>
+        </section>
+    </footer>
 </body>
+
 </html>
--- Periode3-ProjectDishDev/header1.php
+++ Periode3-ProjectDishDev/header1.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vegan Food // Header</title>
     <link rel="stylesheet" href="css/style.css">
-  <!-- Google Fonts -->
+    <!-- Google Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="css/flag-icons.min.css">
 </head>
+
 <body>
     <header>
         <nav class="mainbar">
@@ -41,4 +44,5 @@
         </nav>
     </header>
 </body>
+
 </html>
