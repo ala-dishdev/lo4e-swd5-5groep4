335d4af0a4e1250e5ba3f7af575562dda5adb2cd
Noah Weerwag <6045031@mborijnland.nl>
2025-04-07 16:13:57 +0200

    Update footer
    Footer gefixt, gaf een rare streep aan de zijkanten. Is nu weg.

--- footer.php
+++ footer.php
@@ -5,26 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vegan Food // Footer</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style-noah.css">
     <!-- Google Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="css/flag-icons.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <footer>
+    <footer> 
         <section class="footer-col">
-            <img src="images/beeldmerk_logo_vegan_food.png" alt="Vegan Food Amsterdam">
+            <img src="img/beeldmerk_logo_vegan_food.png" alt="Vegan Food Amsterdam">
         </section>
         <section class="footer-col">
             <h4>Informatie</h4>
-            <article><a href="index.php">Home</a></article>
-            <article><a href="lunch&diner.php">Lunch & Diner</a></article>
-            <article><a href="locatie.php">Openingstijden & Locatie</a></article>
-            <article><a href="reserveren.php">Reserveren</a></article>
-            <article><a href="vacatures.php">Vacatures</a></article>
+            <article><a href="#">Home</a></article>
+            <article><a href="#">Lunch & Diner</a></article>
+            <article><a href="#">Openingstijden & Locatie</a></article>
+            <article><a href="#">Reserveren</a></article>
+            <article><a href="#">Vacatures</a></article>
         </section>
         <section class="footer-col">
             <h4>Openingstijden</h4>
